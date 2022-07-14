7a0a1c73c2e25fd2fdd060b880406c130a9280a0
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-14 00:59:39 -0500

    Update ejercici_18.java

--- ejercici_18.java
+++ ejercici_18.java
@@ -15,6 +15,25 @@ public class ejercici_18 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //HACER UNA APLICACION DE UN NUMERO CELULAR 
+        String celular="";
+        int longitud=0;
+        //PROCESO
+        celular=JOptionPane.showInputDialog("Ingrese su numero celular correctamente");
+        longitud=celular.length();
+        for (int indice = 0; indice < longitud; indice++) {
+            //VALIDACION
+         if (celular.charAt(indice)=='0'|| celular.charAt(indice)=='1'|| celular.charAt(indice)=='2'|| celular.charAt(indice)=='3'||
+              celular.charAt(indice)=='4'||celular.charAt(indice)=='5'|| celular.charAt(indice)=='6'|| celular.charAt(indice)=='7'||
+              celular.charAt(indice)=='8'|| celular.charAt(indice)=='9'){
+             
+        }else{
+             JOptionPane.showMessageDialog(null, "El numero ingresado no esta correcto.");
+                }
+    }
+}
+    }
+
     }
     
 }
