0126cfeaf7a7428ab9105353d2812f76b80e8bf9
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 01:44:22 -0500

    Update ejercici_14.java

--- ejercici_14.java
+++ ejercici_14.java
@@ -15,6 +15,35 @@ public class ejercici_14 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //ENTRADA DE DATOS
+        String palabra="";
+        int longitud =0;
+        int cantidadvocales=0;
+        int cantidadconsonantes=0;
+        
+        //PROCESO
+        palabra = JOptionPane.showInputDialog("Digite la palabra: ");
+        //PROCESO OBTENEMOS LA LONGITUD DE LA PALABRA
+        longitud = palabra.length();
+        //PROCESO DDE CONTAR Y RECORRER Y CONTAR LAS VOCALES Y CONSONANTES 
+        for (int indice =0; indice<longitud; indice++){
+        //VALIDACION DE VOCALES
+        if (palabra.charAt(indice)=='a' || palabra.charAt(indice)=='e' || palabra.charAt(indice)=='i' || palabra.charAt(indice)=='o' ||
+                palabra.charAt(indice)=='u') {
+         cantidadvocales++;   
+        }else{
+            cantidadconsonantes++;
+        }
+        
+        }
+        //SALDIA DE DATOS
+        JOptionPane.showMessageDialog(null, "La cantidad de vocales es: "+cantidadvocales);
+        JOptionPane.showMessageDialog(null, "La cantidad de Consonante es: "+cantidadconsonantes);
+        
+    }
+    
+}
+
     }
     
 }
