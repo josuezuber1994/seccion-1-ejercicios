65d379913ae1e02aff4aa55a2a8cb80049979ca8
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 01:45:01 -0500

    Update ejercci_11.java

--- ejercci_11.java
+++ ejercci_11.java
@@ -14,7 +14,28 @@ public class ejercci_11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ENTRADA DE DATOS POR CONSOLA DE PRODUCTOS POR CONSOLA CANTIDAD DE PRODUCTOS VENTA DE PRODUCTOS 
+        //  RESULTADO FINAL
+        //Entrada de datos
+        Scanner entrada = new Scanner(System.in);
+        String producto = "";
+        int cantidad = 0;
+        float precio, total = 0.0f;
+       
+        System.out.println("------VENTAS DE PRODUCTOS------");//Presentacion por mensaje.
+        
+        //ENTRADA DE DATOS del usuario
+        System.out.print("Ingrese el producto que desea: ");
+        producto = entrada.nextLine();
+        System.out.print("Ingrese la cantidad de producto: ");
+        cantidad =entrada.nextInt();
+        System.out.print("Ingrese el precio del producto: ");
+        precio =entrada.nextFloat();
+        //PROCESO
+        total =cantidad*precio;
+        //Salida de datos
+        System.out.println("El saldo total de cancelar es: $"+total);      
     }
-    
+
 }
+
