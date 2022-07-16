6a243514c791b38900a2f35617efa78d50ac1e8d
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 01:55:41 -0500

    Update ejercici_20.java

--- ejercici_20.java
+++ ejercici_20.java
@@ -14,7 +14,28 @@ public class ejercici_20 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // NUMERO PERFECTO
+        int numero = 4;
+        int antecesores = 0;
+        int acumuladodivisibles = 0;
+        //proceso
+        antecesores = numero - 1;
+        while (antecesores > 0) {
+            if (numero % antecesores == 0) {
+                acumuladodivisibles = acumuladodivisibles + antecesores;
+
+            }
+            antecesores--;
+        }
+        //SALIDA
+        if (numero == acumuladodivisibles) {
+            JOptionPane.showMessageDialog(null, "Es perfecto");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "NO perfecto");
+
+        }
     }
-    
+
 }
+
