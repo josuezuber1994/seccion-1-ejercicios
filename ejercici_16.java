e93403ce7a37db112f616a13a18ed08f1462da08
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 01:54:24 -0500

    Update ejercici_16.java

--- ejercici_16.java
+++ ejercici_16.java
@@ -14,7 +14,19 @@ public class ejercici_16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // HACER UNA APLICACION QUE RECORRA CON NUMEROS LAS PALABRAS MAYUSCULAS
+        String palabra="";
+        int longitud = 0;
+        int indice =0;
+        //Proceso
+        palabra = JOptionPane.showInputDialog("Ingrese una palabra");
+        longitud = palabra.length();
+        do {         
+            JOptionPane.showMessageDialog(null, palabra.toUpperCase().charAt(indice));
+            indice++;
+            
+        } while (indice<longitud);
     }
     
 }
+
