0d7effa8ca565bbee563e6346ed5de1364eb80b1
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 01:54:47 -0500

    Update ejercici_17.java

--- ejercici_17.java
+++ ejercici_17.java
@@ -14,7 +14,27 @@ public class ejercici_17 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //ENTRADA hacer un ejercicio de palabra palindroma 
+        String palabra="";
+        String palabranueva="";
+        int longitud=0;
+        //PROCESO
+        palabra = JOptionPane.showInputDialog("Ingrese una palabra: ");
+        longitud = palabra.length();
+        for (int indice = longitud-1; indice>=0; indice--) {
+            //ARMAMOS LA NUEVA PALABRA
+            
+        palabranueva = palabranueva+palabra.charAt(indice);
+            
+        }
+        //Salida
+        if (palabra.equals(palabranueva)) {
+            JOptionPane.showMessageDialog(null, "Es palindroma");
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "No es polindroma");
+        }
     }
     
 }
+
